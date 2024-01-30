f3f8c52ead5b069a827461b35205087bbaeb7d66
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2024-01-30 17:30:28 +0300

    create migration

---
+++ database/migrations/2024_01_30_134027_create_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(model: User::class)
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignIdFor(model: Group::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->longText('body')
+                ->nullable();
+            $table->foreignId('deleted_by')
+                ->nullable()
+                ->references('id')
+                ->on('users')
+                ->nullOnDelete();
+            $table->timestamp('deleted_at')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
